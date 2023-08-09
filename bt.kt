b0820a0c1e793f0f021564e1af6dd6b140b4443c
Sathya Babu <sathyaBabu@users.noreply.github.com>
2023-08-09 20:28:34 +0530

    Create bt.kt

---
+++ bt.kt
@@ -0,0 +1,13 @@
+
+fun main(args: Array<String>) {
+    println("HelloKotlin!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+    btProtocol()
+}
+fun btProtocol(){
+    println(" this function bt Protocol bt.kt is added in gitHub repository lets pull that in our file system at laptop using git pull..")
+    // fixed the bug by Suresh here....
+}
